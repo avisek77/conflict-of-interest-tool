3e1c2e08c83fbc4568c0fcdabe416e070d873f7f
Ben <160694570+Slorthy@users.noreply.github.com>
2025-08-31 12:26:42 +1000

    Update

---
+++ Project/main.js
@@ -0,0 +1,6 @@
+    <script>
+        function G-0_Checkbox_Change() {
+          checkbox = document.getElementById('Terms_And_Conditions_Checkbox')           
+            // IF The "I Accept" Checkbox isn't selected then the Users can't proceed, possibly an alert could display?
+          }
+    </script>
